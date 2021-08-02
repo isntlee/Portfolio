9a9b8402628f8f7b85954e20824b043eff02bbb4
Space-Rx <leedavidmartina@gmail.com>
2021-08-02 15:38:21 +0000

    cleared out initial program test

--- core/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from allauth.account.views import SignupView
-
-from core.forms import VolunteerSignupForm
-
-class VolunteerSignUpView(SignupView):
-    template_name = 'account/volunteer_signup.html'
-    form_class = VolunteerSignupForm
